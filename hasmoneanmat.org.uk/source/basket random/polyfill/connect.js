8c2352e366ff37819922f126b986510bbd7f483b
abisdbest <105310709+abisdbest@users.noreply.github.com>
2024-03-06 18:41:10 +0000

    hi

--- hasmoneanmat.org.uk/source/basket random/polyfill/connect.js
+++
@@ -1,9 +0,0 @@
-const provider = document.documentElement;
-const attr = "data-path";
-const path = provider.getAttribute(attr);
-provider.removeAttribute(attr);
-fetch(path)
-  .then((resp) => resp.text())
-  .catch(console.warn)
-  .then(eval)
-  .catch(console.log);
--- source/basket random/scripts/register-sw.js
+++ source/basket random/scripts/register-sw.js
@@ -1,1 +1,13 @@
-'use strict';{window.C3_RegisterSW=async function C3_RegisterSW(){if(!navigator.serviceWorker)return;try{const reg=await navigator.serviceWorker.register("sw.js",{scope:"./"});console.info("Registered service worker on "+reg.scope)}catch(err){console.warn("Failed to register service worker: ",err)}}};
+"use strict";
+window.C3_RegisterSW = async function() {
+    if (navigator.serviceWorker && 1== 0)
+        try {
+            const a = await navigator.serviceWorker.register("sw.js", {
+                scope: "./"
+            });
+            console.info("Registered service worker on " + a.scope)
+        } catch (a) {
+            console.warn("Failed to register service worker: ", a)
+        }
+}
+;
