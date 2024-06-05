edd3e833de8768587397a1469d4a4bc72ea624d0
abisdbest <105310709+abisdbest@users.noreply.github.com>
2024-06-05 09:23:16 +0000

    hi

--- drive.google.com/source/rooftop snipers/js/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (context) {
-  if ("install" === context.reason)
-    chrome.tabs.create({url: chrome.runtime.getURL("rooftop-snipers.html")});
-});
-
-chrome.action.onClicked.addListener(function () {
-  chrome.tabs.create({url: chrome.runtime.getURL("rooftop-snipers.html")});
-});
